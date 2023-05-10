983b3dc167f3d42c26654f49cfd899f808ff7cc8
kmatiya <khormatiya@gmail.com>
2023-05-10 15:15:14 +0200

    Refactor repository to handle more than one report (Generic)

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
+import json
+
 import pandas as pd
 import requests
 from requests.auth import HTTPBasicAuth
-
-clerks_df = pd.read_excel('./IC3-Clerks.xlsx', header=0)
-ndpClerkUrl = "https://neno.pih-emr.org/dhis/api/dataValueSets"
-username = "pih_dhis2_user"
-pwd = "pih_dhis_password"
-
-dataSetDictionary = {
-    "Number of DBS samples rejected": "NX9ITIOkqCw",
-    "Number of exposed infants registered": "poyokFLTPMo",
-    "Number of HIV exposed infants born": "WYUB8EK3P02",
-    "Number of lab results received and entered in the viral load register": "nyj0DMQn5kk",
-    "Number of mastercards requested": "d0SdXKBG0Rv",
-    "Number of patients entered in the TRACE update forms": "GZlIeseXO8P",
-    "Number of patients that had visited after their appointment on a non IC3 clinic day": "wO1eI3ZhtiF",
-    "Number of viral load plasma samples rejected": "gVP7uekbIS2",
-    "Number of viral load requests filled": "OyGjf17Iht6",
-    "Number of exposed infants received": "EAwRze4dQ58"
-}
+from pihmalawi_config import config
+from datetime import datetime
 
 
 def get_org_unit(facility):
@@ -54,63 +40,43 @@ def get_org_unit(facility):
 
 
 if __name__ == '__main__':
-    print(clerks_df.head())
-    clerks_df["orgUnit"] = clerks_df["Facility"].apply(get_org_unit)
-    for index, row in clerks_df.iterrows():
-        facility_report = {
-            "dataSet": "XttTY8YbYKB",
-            "completeDate": "2021-02-16",
-            "period": row["period"],
-            "orgUnit": row["orgUnit"],
-            "dataValues": [
-                {
-                    "dataElement": dataSetDictionary["Number of viral load requests filled"],
-                    "value": row["# of Total VL request filled"]
-                },
-                {
-                    "dataElement": dataSetDictionary[
-                        "Number of lab results received and entered in the viral load register"],
-                    "value": row["# of Lab results received and entered in the VL register"]
-                },
-                {
-                    "dataElement": dataSetDictionary["Number of patients entered in the TRACE update forms"],
-                    "value": row["# of Patients entered in TRACE update form"]
-                },
-                {
-                    "dataElement": dataSetDictionary[
-                        "Number of patients that had visited after their appointment on a non IC3 clinic day"],
-                    "value": row["# of Patients that had visited after their appointment on a non-IC3 day"]
-                },
-                {
-                    "dataElement": dataSetDictionary["Number of mastercards requested"],
-                    "value": row["# of master cards requested"]
-                },
-                {
-                    "dataElement": dataSetDictionary["Number of HIV exposed infants born"],
-                    "value": row["# of HIV exposed infants born (Maternity register)"]
-                },
-                {
-                    "dataElement": dataSetDictionary["Number of exposed infants registered"],
-                    "value": row["# of exposed registered this month (EID Register)"]
-                },
-                {
-                    "dataElement": dataSetDictionary["Number of DBS samples rejected"],
-                    "value": row["# of DBS sample rejected"]
-                },
-                {
-                    "dataElement": dataSetDictionary["Number of viral load plasma samples rejected"],
-                    "value": row["# of VL Plasma sample rejected"]
-                },
-                {
-                    "dataElement": dataSetDictionary["Number of exposed infants received"],
-                    "value": row["# of  Exposed infants received"]
-                }
-            ]
-        }
-        post_result = requests.post(ndpClerkUrl, json=facility_report,
-                                    auth=HTTPBasicAuth(username=username, password=pwd))
-        if post_result.status_code == 200:
-            print(post_result.text)
-            print("Report for " + row["Facility"] + " has been added in NDP")
-        else:
-            print("Error loading " + row["Facility"] + ". " + post_result.text)
+    for each_endpoint in config["endpoints"]:
+        report_config_df = pd.read_csv(each_endpoint["config_file"])
+        report_df = pd.read_excel(each_endpoint["report_file"], header=0)
+        report_df["orgUnit"] = report_df["Facility"].apply(get_org_unit)
+        print(report_df.head())
+        for index, row in report_df.iterrows():
+
+            facility_report = {
+                "dataSet": report_config_df["dataset"].iat[0],
+                "completeDate": str(datetime.today().date()),
+                "period": row["period"],
+                "orgUnit": row["orgUnit"],
+                "dataValues": []
+            }
+            for idx, each_config in report_config_df.iterrows():
+                column_name = each_config["excel_column"]
+                if column_name in row:
+                    data_element = {
+                        "dataElement": each_config["dataset_element_id"],
+                        "value": row[column_name]
+                    }
+                    facility_report["dataValues"].append(data_element)
+                else:
+                    data_element = {
+                        "dataElement": each_config["dataset_element_id"],
+                        "value": None,
+                    }
+                    facility_report["dataValues"].append(data_element)
+
+            print(facility_report)
+            url = each_endpoint["base"] + report_config_df["resource"].iat[0]
+            username = each_endpoint["username"]
+            password = each_endpoint["password"]
+            post_result = requests.post(url, json=facility_report,
+                                        auth=HTTPBasicAuth(username=username, password=password))
+            if post_result.status_code == 200:
+                print(post_result.text)
+                print("Report for " + row["Facility"] + " has been added in NDP")
+            else:
+                print("Error loading " + row["Facility"] + ". " + post_result.text)
---
+++ pihmalawi_config.py
@@ -0,0 +1,11 @@
+config = {
+    "endpoints": [
+        {
+            "base": "https://neno.pih-emr.org/dhis/api",
+            "username": "",
+            "password": "",
+            "report_file": "IC3-Clerks.xlsx",
+            "config_file": "clerks_config.csv",
+        },
+    ],
+}
