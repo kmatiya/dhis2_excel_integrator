38a2e9b474924e9742265fb9db4eae46e7143c43
Tim <kavumatimothy1@gmail.com>
2024-03-14 19:59:02 +0200

    I have modified the event capture report to update already existing reports and enter new reports where no reports are entered for the dates on the Facility report

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from dataCapture import dataCapture
-from eventCapture import eventCapture
 from pihmalawi_config import config
+from eventCapture import EventManager
+
 
 def get_report_type():
     for each_endpoint in config["endpoints"]:
@@ -9,7 +10,17 @@ def get_report_type():
         if report_config_df["resource"][0] == "/dataValueSets":
             dataCapture()
         if report_config_df["resource"][0] == "/events":
-            eventCapture()
+            print("starting Event manager")
+            event_manager = EventManager(config)
+            print("Checking for already entered reports with the same date")
+            event_ids = event_manager.entered_events()
+            if len(event_ids) != 0:
+                print("There are Reports for the dates provided: Preparing to update existing reports")
+                event_manager.update_event()
+            else:
+                print("Uploading Reports to NDP")
+                event_manager.upload_new_event()
+
 
 if __name__ == '__main__':
     get_report_type()
